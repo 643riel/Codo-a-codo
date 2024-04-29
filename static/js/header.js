b1f78f0ce8c6d8e55868839af1cb43ca32d15271
643riel <139085506+643riel@users.noreply.github.com>
2024-04-28 22:55:27 -0300

    Agregado header desde script
    Se creó el script de header usando ... para que sirva para todos los html sin tener que repetir el código

---
+++ static/js/header.js
@@ -0,0 +1,20 @@
+// Contenido del encabezado
+var headerContent = `
+<!-- 
+En las rutas usa .. para quedar en raíz. 
+Esto es para que sin importar donde estoy parado, encuentre el archivo.
+-->
+
+<header class="header">
+<nav class="en-linea">
+    <a class="hipervinculo" href="../index.html">Inicio</a>
+    <a class="hipervinculo" href="../template/nosotros.html">Sobre nosotros</a>
+    <a class="hipervinculo" href="../template/promos.html">Promociones</a>
+    <a class="hipervinculo" href="../template/form.html">Formulario de contacto</a>
+    <a class="hipervinculo" href="https://www.google.com/" target="_blank">Google</a>
+</nav>
+</header>
+`;
+
+// Agregar el contenido del encabezado al principio del body
+document.body.insertAdjacentHTML('afterbegin', headerContent);

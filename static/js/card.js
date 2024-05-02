ec305ccf3abca89432813ef1897d3112260e922c
alfredo <castroalfred89@gmail.com>
2024-05-02 11:08:14 -0300

    agrego clase en promo.css para precios y agrego en la funcion metodos para crear precios y verlo debajo de la img

--- static/js/card.js
+++ static/js/card.js
@@ -1,5 +1,5 @@
 // Función para crear una nueva card con descripción y una imagen
-function createCard(descripcion, imageUrl) {
+function createCard(descripcion, imageUrl, precio) {
     // Crear elemento div para la card
     var card = document.createElement('div');
     card.classList.add('card');
@@ -15,9 +15,16 @@ function createCard(descripcion, imageUrl) {
     image.alt = 'Imagen';
     image.classList.add('image');
 
+    // Crear elemento para la venta 
+    var price = document.createElement('p');
+    price.textContent = precio;
+    price.innerHTML = precio; // esta propiedad me permite colocar un salto de linea con br en el texto que ingrese
+    price.classList.add('price');
+
     // Agregar descripción e imagen a la card
     card.appendChild(description);
     card.appendChild(image);
+    card.appendChild(price);
 
     return card;
 }
@@ -27,9 +34,9 @@ var cardContainer = document.getElementById('card-container');
 
 // Crear cards y agregarlas al contenedor
 var cards = [
-    createCard('Cerveza artesanal alemana', '../static/img/artesanal.jpg'),
-    createCard('Ginebra alemana', '../static/img/gin2.jpg'),
-    createCard('Licor aleman', '../static/img/licor-aleman.jpg'),
+    createCard('Cerveza artesanal alemana', '../static/img/artesanal.jpg', 'Chop Rubia: U$10 <br> Chop Negra: U$15 <br> Chop Roja: U$15'),
+    createCard('Ginebra alemana', '../static/img/gin2.jpg', 'Gin-Tonic: U$15 <br> Shot: U$5 <br> Botella Hamburg Zanzibar: U$30'),
+    createCard('Licor aleman', '../static/img/licor-aleman.jpg', 'Shot: U$7 <br> Botella Jägermeister: U$35'),
     createCard('Descripción de ejemplo2', 'ejemplo.jpg')
 ];
 

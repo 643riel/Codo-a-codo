81486e0efa9a24b4812ca6531a60816b843142b9
alfredo <castroalfred89@gmail.com>
2024-05-03 10:20:37 -0300

    agrego img a promo con descrip

--- static/js/card.js
+++ static/js/card.js
@@ -34,10 +34,12 @@ var cardContainer = document.getElementById('card-container');
 
 // Crear cards y agregarlas al contenedor
 var cards = [
-    createCard('Cerveza artesanal alemana', '../static/img/artesanal.jpg', '¡Descubre la auténtica esencia de la cerveza alemana con nuestra selección de importación! Sumérgete en la rica tradición cervecera de Alemania y déjate seducir por el sabor incomparable de nuestras cervezas cuidadosamente seleccionadas:<br><br>Chop Rubia: U$10 <br><br> Chop Negra: U$15 <br><br> Chop Roja: U$15'),
-    createCard('Ginebra alemana', '../static/img/gin2.jpg', 'Inspirada en la vibrante fusión de culturas de la histórica ciudad portuaria de Hamburgo y la exótica isla de Zanzíbar, esta ginebra combina lo mejor de dos mundos en una botella:<br><br>Gin-Tonic: U$15 <br><br> Shot: U$5 <br><br> Botella Hamburg Zanzibar: U$30'),
-    createCard('Licor aleman', '../static/img/licor-aleman.jpg', ' Desde su creación en la mística Selva Negra de Alemania, Jägermeister ha cautivado a los paladares más exigentes con su distintivo sabor a base de hierbas y especias. Sumérgete en el emblemático mundo de Jägermeister, donde la tradición se encuentra con la innovación en cada gota:<br><br>Shot: U$7 <br><br> Botella Jägermeister: U$35'),
-    createCard('Descripción de ejemplo2', 'ejemplo.jpg')
+    createCard('Cerveza artesanal alemana', '../static/img/artesanal.jpg', '¡Descubre la auténtica esencia de la cerveza alemana con nuestra selección de importación! Sumérgete en la rica tradición cervecera de Alemania y déjate seducir por el sabor incomparable de nuestras cervezas cuidadosamente seleccionadas:<br><br>Chop Rubia: US$10 <br><br> Chop Negra: US$15 <br><br> Chop Roja: US$15'),
+    createCard('Ginebra alemana', '../static/img/gin2.jpg', 'Inspirada en la vibrante fusión de culturas de la histórica ciudad portuaria de Hamburgo y la exótica isla de Zanzíbar, esta ginebra combina lo mejor de dos mundos en una botella:<br><br>Gin-Tonic: US$15 <br><br> Shot: US$5 <br><br> Botella Hamburg Zanzibar: US$30'),
+    createCard('Licor aleman', '../static/img/licor-aleman.jpg', ' Desde su creación en la mística Selva Negra de Alemania, Jägermeister ha cautivado a los paladares más exigentes con su distintivo sabor a base de hierbas y especias. Sumérgete en el emblemático mundo de Jägermeister, donde la tradición se encuentra con la innovación en cada gota:<br><br>Shot: US$7 <br><br> Botella Jägermeister: US$35'),
+    createCard('Salchichas alemanas', '../static/img/salchicha.jpg', 'Las salchichas alemanas son una deliciosa tradición culinaria que ha trascendido fronteras y conquistado paladares en todo el mundo. Con su distintivo sabor ahumado y su textura jugosa, estas salchichas son el epítome de la excelencia gastronómica alemana:<br><br>Salchichas alemanas al vino: US$15<br><br>Hot Dog: US$10'),
+    createCard('Choripan', '../static/img/chori.jpg', 'Originarios de Argentina, estos deliciosos bocados combinan la intensidad de la carne de chorizo con la suavidad del pan criollo. Cada mordisco es una fiesta para los sentidos, con el aroma tentador de las especias y el sabor ahumado de la carne a la parrilla'),
+    createCard('Descripción de ejemplo2', 'ejemplo.jpg'),
 ];
 
 cards.forEach(function(card) {

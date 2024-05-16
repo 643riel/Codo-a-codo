885ab209c779f56d42d0b2505731876ae72d6bce
643riel <139085506+643riel@users.noreply.github.com>
2024-05-16 10:06:18 -0300

    creado promos.json
    Ahora la info para las cards sale de un json

--- static/js/card.js
+++ static/js/card.js
@@ -15,13 +15,12 @@ function createCard(descripcion, imageUrl, precio) {
     image.alt = 'Imagen';
     image.classList.add('image');
 
-    // Crear elemento para la venta 
+    // Crear elemento para el precio
     var price = document.createElement('p');
-    price.textContent = precio;
-    price.innerHTML = precio; // esta propiedad me permite colocar un salto de linea con br en el texto que ingrese
+    price.innerHTML = precio; // permite usar código html
     price.classList.add('price');
 
-    // Agregar descripción e imagen a la card
+    // Agregar descripción, imagen y precio a la card
     card.appendChild(description);
     card.appendChild(image);
     card.appendChild(price);
@@ -32,16 +31,14 @@ function createCard(descripcion, imageUrl, precio) {
 // Obtener el contenedor de las cards
 var cardContainer = document.getElementById('card-container');
 
-// Crear cards y agregarlas al contenedor
-var cards = [
-    createCard('Cerveza artesanal alemana', '../static/img/artesanal.jpg', '¡Descubre la auténtica esencia de la cerveza alemana con nuestra selección de importación! Sumérgete en la rica tradición cervecera de Alemania y déjate seducir por el sabor incomparable de nuestras cervezas cuidadosamente seleccionadas:<br><br><b><i>Chop Rubia</i></b>: US$10 <br><br> <b><i>Chop Negra</i></b>: US$15 <br><br><b><i>Chop Roja</i></b>: US$15'),
-    createCard('Ginebra alemana', '../static/img/gin2.jpg', 'Inspirada en la vibrante fusión de culturas de la histórica ciudad portuaria de Hamburgo y la exótica isla de Zanzíbar, esta ginebra combina lo mejor de dos mundos en una botella:<br><br><b><i>Gin-Tonic</i></b>: US$15 <br><br><b><i>Shot</i></b>: US$5 <br><br><b><i>Botella Hamburg Zanzibar</i></b>: US$30'),
-    createCard('Licor aleman', '../static/img/licor-aleman.jpg', ' Desde su creación en la mística Selva Negra de Alemania, Jägermeister ha cautivado a los paladares más exigentes con su distintivo sabor a base de hierbas y especias. Sumérgete en el emblemático mundo de Jägermeister, donde la tradición se encuentra con la innovación en cada gota:<br><br><b><i>Shot</i></b>: US$7 <br><br><b><i>Botella Jägermeister</i></b>: US$35'),
-    createCard('Salchichas alemanas', '../static/img/salchicha.jpg', 'Las salchichas alemanas son una deliciosa tradición culinaria que ha trascendido fronteras y conquistado paladares en todo el mundo. Con su distintivo sabor ahumado y su textura jugosa, estas salchichas son el epítome de la excelencia gastronómica alemana:<br><br><b><i>Salchichas alemanas al vino</i></b>: US$15<br><br><b><i>Hot Dog</i></b>: US$10'),
-    createCard('Choripan', '../static/img/chori.jpg', 'Originarios de Argentina, estos deliciosos bocados combinan la intensidad de la carne de chorizo con la suavidad del pan criollo. Cada mordisco es una fiesta para los sentidos, con el aroma tentador de las especias y el sabor ahumado de la carne a la parrilla<br><br><b><i>Chori Completo</i></b>: US$10<br><br><b><i>Wunderchori</i></b>: US$14'),
-    createCard('Tabla de fiambres', '../static/img/picada.jpg', 'En nuestra tabla de fiambres, te invitamos a disfrutar de una selección cuidadosamente elaborada de los más exquisitos embutidos y quesos que nuestra tierra tiene para ofrecer. Desde el auténtico salame argentino hasta el sabor intenso del queso criollo, cada bocado es un viaje sensorial que te transportará a las pampas argentinas:<br><br><b><i>Para 2</i></b>: US$10<br><br><b><i>Para 4</i></b>: US$15'),
-];
-
-cards.forEach(function(card) {
-    cardContainer.appendChild(card);
-});
+// Cargar el archivo JSON promos.json
+fetch('../static/js/promos.json') // ahora la info de cada card está acá
+    .then(response => response.json())
+    .then(cardsData => {
+        // Crear cards y agregarlas al contenedor
+        cardsData.forEach(function(cardData) {
+            var card = createCard(cardData.descripcion, cardData.imageUrl, cardData.precio); // función de arriba
+            cardContainer.appendChild(card);
+        });
+    })
+    .catch(error => console.error('Error al cargar el archivo JSON:', error));
